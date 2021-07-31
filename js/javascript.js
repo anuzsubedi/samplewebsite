2cc3fa7e15ec42d1f7876754cc48d43fdd39701c
anuzsubedi <anuzsubedi@hotmail.com>
2021-08-01 00:44:16 +0545

    First Commit

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+function changepersonal()
+{
+	var element = document.getElementById('text');
+	element.innerHTML =
+	 "Technology has affected a lot in my personal life. It has changed everything I do to a mico level. The things I am most interested in are gadagets. Evrything related to technology affects me and picks up my interested. I am hugely interested in the vehicles of Tesla Motocorp because they are packed with technology. <br> Technology has affected my interest, it has affected the media content I consume and it has even affected my hobbies. I like to play video games rather than physical sports because I am good at computer games rather than sports. Technology has made a huge impact on my personal life.				  ";
+}
+function changestudent()
+{
+	var element = document.getElementById('text');
+	element.innerHTML = "Technology has affected my student life a lot too. I have been interested in computer and computer science subject from a very early age. I used to do learn qbasic form my sister's coursebook when she was in grade 6 and I was in grade 2. Now, even in undergraduate level my choice of study has been affected by technology. If technology had not advanced the way it has today, there would have been no course related to computing and I wouldn't have been able to read it as well. Even inside the course, we have been using gadgets to access cours materials and to do and submit coursework. Technology has made a huge impact in my student lfe";
+}
+function changesocial()
+{
+	var element = document.getElementById('text');
+	element.innerHTML = "Technology has affected almost everybody's social life. People's form of communication has drasticlaly changed in the last 3 to 4 decades. Through social media platforms people are able to connect to unknown people as well. Poeple living in Nepal can have close friends in Japan and vice-versa. It also has some bad effects. People limit themselves to the four walls of their room and rarely go out to play and be active socially. People from far away are becoming close but people living in a close vicinity have become far from each other.";
+}
